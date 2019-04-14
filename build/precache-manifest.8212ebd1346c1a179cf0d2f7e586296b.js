3a6d8c487d158c12f9ed6938612f4411d2f4f048
kam <anthony.kam@epitech.eu>
2019-04-14 15:23:14 +0200

    [TEST] you can't read this too

---
+++ build/precache-manifest.8212ebd1346c1a179cf0d2f7e586296b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "ec0efbc046489aa1e466f75b4aa5bffa",
+    "url": "/static/media/backgroundNavbar.ec0efbc0.jpg"
+  },
+  {
+    "revision": "142ebf4e629e5f699d4abeb04a571fba",
+    "url": "/static/media/background.142ebf4e.jpg"
+  },
+  {
+    "revision": "97f8131237776e7c6caedd0918335ba3",
+    "url": "/static/media/Milk.97f81312.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "46d4525285967f54de20",
+    "url": "/static/js/main.57582ffd.chunk.js"
+  },
+  {
+    "revision": "4a23d037a57d1566d990",
+    "url": "/static/js/2.5c9bf62d.chunk.js"
+  },
+  {
+    "revision": "46d4525285967f54de20",
+    "url": "/static/css/main.754e18e2.chunk.css"
+  },
+  {
+    "revision": "4b440a2f14151ea8db17b1cb294e89fa",
+    "url": "/index.html"
+  }
+];
