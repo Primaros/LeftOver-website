e63b5fc78d1c58b857a1ac6a897987083c0301ed
kam <anthony.kam@epitech.eu>
2019-04-14 22:49:22 +0200

    [ADD] title & icon change

--- src/App.jsx
+++ src/App.jsx
@@ -9,21 +9,8 @@ import NotFound from './containers/errors/NotFound';
 import ConnectionError from './containers/errors/ConnectionError';
 import './App.css';
 
-const conf = () => (
-  <div>
-    <link
-      rel="stylesheet"
-      href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-      integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
-      crossOrigin="anonymous"
-    />
-    <title>LeftOver</title>
-  </div>
-);
-
 const App = () => (
   <Provider store={Store}>
-    { conf() }
     <Router history={History}>
       <Switch>
         <Route exact path="/" component={Landing} />
